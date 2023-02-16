b12857274c0a636aa75dbd50abdb745acc166fdb
KlevisonF <silvaklevison11@gmail.com>
2023-02-16 01:49:30 -0300

    first commit

---
+++ src/data/word.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+    Carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["banana","Maçã", "Pêra", "laranja"],
+    Corpo: ["Braço", "Perna", "Cêrebro","Pescoço", "Olhos"],
+    computador: ["Mause","Teclado","Monitor","Gabinete"],
+    programação:["Linguagem","Fremeword","JavaScript","React"],
+    alimento:["Ovo","Arroz","Feijao","Leite"]
+
+};
